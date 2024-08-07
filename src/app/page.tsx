93ccfadc43b2bb3c8b828ddc4660d11c233ba95a
Shobika Saravanan <shobikasaravanan1998@gmail.com>
2024-08-07 16:49:40 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="text-3xl	text-center font-bold"><div>Build blazingly fast web apps with</div>  <div className="mt-3 font-extrabold text-red-400">Think UI</div></div>
       <p className="text-center text-lg mt-10">Mind UI is a lightweight and modular component library which helps developers to build beautiful web apps with speed. Copy the below code to use Think UI in your project.</p>
       <div className="flex justify-center items-center mt-10">
-        <Link className="rounded-lg bg-red-400 hover:bg-red-500 text-white text-xl p-5" href="./alert">Get Started =></Link>
+        <Link className="rounded-lg bg-red-400 hover:bg-red-500 text-white text-xl p-5" href="./alert">Get Started</Link>
       </div>
 
       <div className="w-3/4 h-48 bg-red-100 mt-10 flex justify-center items-center">
